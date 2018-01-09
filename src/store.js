c5b34859e34b55d0e8bbe7a862eb2b5e7da1abbb
Lucas Everett <lucas@lucaseverett.codes>
2018-01-09 13:13:25 -0600

    Filter bookmarks

--- src/store.js
+++ src/store.js
@@ -5,11 +5,9 @@ const data = () => {
     bookmarks = await browser.bookmarks.getChildren("toolbar_____");
 
     try {
-      bookmarks = bookmarks.map(({ title, url, type }) => {
-        if (!/^place:sort=/.test(url) && type !== "folder") {
-          return { title, url };
-        }
-      });
+      bookmarks = bookmarks
+        .filter(({ url, type }) => !/^place:/.test(url) && type === "bookmark")
+        .map(({ title, url }) => ({ title, url }));
     } catch (e) {
       console.log(`Error: ${e}`);
     }
