f7d976773fe2b8045ce848a87d8883fae9a068ec
Lucas Everett <lucas@lucaseverett.codes>
2018-01-08 23:01:12 -0600

    Padding/formatting

--- src/components/index.js
+++ src/components/index.js
@@ -17,24 +17,36 @@ const display = bookmarks => {
       shades: ["700"],
       text: name.join(".")
     });
-    if (name.length === 3) {
+    if (name.length === 3 && name[0].length < name[1].length) {
       name = name.map((n, i) => {
         if (i === 0) {
           return `<div class="small align-left">${n}</div>`;
         } else if (i + 1 === name.length) {
           return `<div class="small align-right">${n}</div>`;
         }
-        return `<div>${n}</div>`;
+        return `<div class="domain">${n}</div>`;
       });
     } else if (name.length === 2) {
       name = name.map((n, i) => {
         if (i === 0) {
-          return `<div class="two">${n}</div>`;
+          return `<div class="domain two">${n}</div>`;
         }
         return `<div class="small align-right">${n}</div>`;
       });
+    } else if (
+      name[0].length > name[1].length ||
+      name[0].length === name[1].length
+    ) {
+      name = html`<div class="domain two">${
+        name[0]
+      }</div><div class="small align-right">${name
+        .splice(1)
+        .map(n => `${n}`)
+        .join(".")}</div>`;
     } else {
-      name = html`<div class="small align-left">${name[0]}</div><div>${
+      name = html`<div class="small align-left">${
+        name[0]
+      }</div><div class="domain">${
         name[1]
       }</div><div class="small align-right">${name
         .splice(2)
