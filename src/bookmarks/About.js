0308a45e197331c02f6d3521d0af181b474506f0
Lucas Everett <lucas@lucaseverett.dev>
2021-04-14 21:01:20 -0500

    'About' changes

--- src/bookmarks/About.js
+++ src/bookmarks/About.js
@@ -3,9 +3,18 @@ import logo from "url:../../public/icons/128.svg";
 import { appVersion } from "../common/version.js";
 
 const styles = css`
-  display: flex;
-  align-items: center;
   line-height: 1.5;
+
+  .about {
+    display: flex;
+    align-items: center;
+  }
+
+  .copyright {
+    font-size: 12px;
+    text-align: center;
+    margin-top: 25px;
+  }
 
   img {
     padding-right: 20px;
@@ -33,31 +42,45 @@ const styles = css`
 
 export const About = () => (
   <div className={styles}>
-    <img src={logo} />
-    <div>
-      <p>Version {appVersion}</p>
-      <p>
-        <a href="https://toolbardial.com/" rel="noopener" target="_blank">
-          Toolbar Dial
-        </a>{" "}
-        was created by{" "}
-        <a href="https://lucaseverett.dev/" rel="noopener" target="_blank">
-          Lucas Everett
-        </a>
-        .
-      </p>
-      <p>
-        Please report issues and submit requests to the{" "}
-        <a
-          href="https://github.com/lucaseverett/toolbar-dial"
-          rel="noopener"
-          target="_blank"
-          id="github-repo-link"
-        >
-          GitHub repository
-        </a>
-        .
-      </p>
+    <div className="about">
+      <img src={logo} />
+      <div>
+        <p>Version {appVersion}</p>
+        <p>
+          <a href="https://toolbardial.com/" rel="noopener" target="_blank">
+            Toolbar Dial
+          </a>{" "}
+          was created by{" "}
+          <a href="https://lucaseverett.dev/" rel="noopener" target="_blank">
+            Lucas Everett
+          </a>
+          .
+        </p>
+        <p>
+          Please report issues and submit requests to the{" "}
+          <a
+            href="https://github.com/lucaseverett/toolbar-dial"
+            rel="noopener"
+            target="_blank"
+            id="github-repo-link"
+          >
+            GitHub repository
+          </a>
+          .
+        </p>
+      </div>
+    </div>
+    <div className="copyright">
+      Copyright &copy; {new Date().getFullYear()} Lucas Everett. Released under
+      the{" "}
+      <a
+        href="https://opensource.org/licenses/MIT"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        MIT License
+      </a>
+      .
     </div>
   </div>
 );
